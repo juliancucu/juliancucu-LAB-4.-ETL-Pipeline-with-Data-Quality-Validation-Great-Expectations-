e3bb01bbe6eacaac23294e0c01c21252642f1152
juliancucu <julian_david.cuellar@uao.edu.co>
2026-03-25 14:57:26 -0500

    transform.py

--- src/transform.py
+++ src/transform.py
@@ -9,20 +9,19 @@ def transform_data(df):
     # Nos aseguramos de que la fecha sea tipo datetime
     df_transformed['invoice_date'] = pd.to_datetime(df_transformed['invoice_date'])
 
-    # 1. Extraer características de la fecha
+    # Extraer características de la fecha
     df_transformed['year'] = df_transformed['invoice_date'].dt.year
     df_transformed['month'] = df_transformed['invoice_date'].dt.month
     df_transformed['day'] = df_transformed['invoice_date'].dt.day
     df_transformed['quarter'] = df_transformed['invoice_date'].dt.quarter
 
-    # 2. Crear una categoría de ingresos (Revenue Category)
-    # Por ejemplo: Low (< $500), Medium ($500 - $2000), High (> $2000)
+    # Crear una categoría de ingresos 
     bins = [-float('inf'), 500, 2000, float('inf')]
     labels = ['Low', 'Medium', 'High']
     df_transformed['revenue_category'] = pd.cut(df_transformed['total_revenue'], bins=bins, labels=labels)
 
-    # 3. Categoría de precio unitario (Unit Price Category)
-    # Por ejemplo: Budget (< $100), Standard ($100 - $500), Premium (> $500)
+    # Categoría de precio unitario 
+    
     price_bins = [-float('inf'), 100, 500, float('inf')]
     price_labels = ['Budget', 'Standard', 'Premium']
     df_transformed['price_category'] = pd.cut(df_transformed['price'], bins=price_bins, labels=price_labels)
