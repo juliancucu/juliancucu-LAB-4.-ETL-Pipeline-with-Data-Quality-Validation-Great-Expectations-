0af5ac16912488f4ba001f5cc4ecb389337eda6f
juliancucu <julian_david.cuellar@uao.edu.co>
2026-03-26 00:03:28 -0500

    Update data cleaning to remove duplicates by product

--- src/clean.py
+++ src/clean.py
@@ -7,10 +7,10 @@ def clean_data(df):
     # Trabajamos sobre una copia para no alterar el original
     df_clean = df.copy()
 
-    # Eliminar duplicados basados en el invoice_id
-    df_clean = df_clean.drop_duplicates(subset=['invoice_id'], keep='first')
+    # Eliminar solo si tienen el mismo ID de factura, el mismo producto y la misma cantidad.
+    df_clean = df_clean.drop_duplicates(subset=['invoice_id', 'product', 'quantity'], keep='first')
 
-    # COMPLETITUD E IMPUTACIÓN: 
+    
     # Crear IDs únicos y secuenciales para los nulos
     max_id = df_clean['customer_id'].max()
     if pd.isna(max_id):
@@ -23,28 +23,29 @@ def clean_data(df):
         nuevos_ids = np.arange(max_id + 1, max_id + 1 + num_missing)
         df_clean.loc[missing_customers_mask, 'customer_id'] = nuevos_ids
 
-    # Rellenamos nulos con la transacción anterior/siguiente
+    # 2. invoice_date (Nulos iniciales): Rellenamos nulos con la transacción anterior/siguiente
     df_clean['invoice_date'] = df_clean['invoice_date'].ffill().bfill()
 
     # Arreglar cantidades y precios
+   
     df_clean = df_clean[df_clean['price'] >= 0.01]
     df_clean = df_clean[df_clean['quantity'] >= 1]
 
     # Estandarizar nombres de países
-    # Convertimos todo a minúsculas y luego la primera letra en mayúscula 
+    # Convertimos todo a minúsculas y luego la primera letra en mayúscula (ej. 'ecuador' -> 'Ecuador')
     df_clean['country'] = df_clean['country'].str.title()
     # Reemplazamos abreviaturas
     country_map = {'Co': 'Colombia', 'Pe': 'Peru', 'Ec': 'Ecuador', 'Cl': 'Chile'}
     df_clean['country'] = df_clean['country'].replace(country_map)
 
-    #Arreglar fechas
+    # Arreglar fechas
     # Convertimos todas las fechas mezcladas a un objeto datetime real de pandas
     df_clean['invoice_date'] = pd.to_datetime(df_clean['invoice_date'], format="mixed", errors='coerce')
     
     # Por si algún formato raro falló y se volvió NaT al parsear, aplicamos ffill de nuevo
     df_clean['invoice_date'] = df_clean['invoice_date'].ffill().bfill()
     
-    # Imputación de Fechas Futuras: Las "topamos" a la fecha máxima de corte (2023-12-31)
+    # Imputación de Fechas Futuras, Las "topamos" a la fecha máxima de corte (2023-12-31)
     max_valid_date = pd.Timestamp('2023-12-31')
     df_clean.loc[df_clean['invoice_date'] > max_valid_date, 'invoice_date'] = max_valid_date
     
